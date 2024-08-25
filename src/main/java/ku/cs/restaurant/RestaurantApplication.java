c758d15b07e1fb2aee6470559105b2f5001d2c2f
Moradop Hengprasert <moradop1201@gmail.com>
2024-08-25 20:13:46 +0700

    initial commit

---
+++ src/main/java/ku/cs/restaurant/RestaurantApplication.java
@@ -0,0 +1,18 @@
+/*
+ * StudentID: 6510450861
+ * Name: Moradop Hengprasert
+ * */
+
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
